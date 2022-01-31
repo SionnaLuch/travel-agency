d32d8906292cdb4b48ad14e74873715c2de68cfa
Sionna Poleszuk <sionna.pol@gmail.com>
2022-01-31 11:40:53 +0100

    order option

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,21 +3,29 @@ import {Grid,Row,Col} from 'react-flexbox-grid';
 import OrderOption from '../OrderOption/OrderOption';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
+import PropTypes from 'prop-types';
 
 
-const OrderForm = ({cost,id}) =>(
+const OrderForm = ({tripCost, options}) =>{
+    return (
 <Grid>
   <Row>
       {pricing.map(options =>(
-          <Col md={4} key={id}>
+          <Col md={4} key={options.id}>
               <OrderOption {...options} /> 
           </Col>
       ))};
     <Col xs={12}>
-      <OrderSummary tripCost={cost} tripOptions={pricing}/>
+      <OrderSummary cost={tripCost} options={options}/>
     </Col>
   </Row>
 </Grid>
-);
+    );
+};
+OrderForm.propTypes = {
+    cost: PropTypes.string,
+    options:PropTypes.object,
+  
+}
 
 export default OrderForm;
--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import styles from './OrderSummary.module.scss';
-import calculateTotal from'../../../utils/calculateTotal';
-import formatPrice from '../../../utils/formatPrice';
+import {calculateTotal} from'../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
 
 const OrderSummary = (props)=> {
     return( 
         <div>
             <h2 className={styles.component}>
             Total:
-                <strong>{formatPrice(calculateTotal(props.tripCost.tripCost, props.tripCost.options))}</strong>
+                <strong>{formatPrice(calculateTotal(props.cost, props.options))}</strong>
             </h2>
         </div>
     );
-}
-
+};
+OrderSummary.propTypes = {
+    options: PropTypes.object,
+    cost:PropTypes.string,
+};
 export default OrderSummary;
