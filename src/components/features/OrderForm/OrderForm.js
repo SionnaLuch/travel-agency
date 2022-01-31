10282105de63ec8ca4c769a179cc33f7d9edc62f
Sionna Poleszuk <sionna.pol@gmail.com>
2022-01-31 10:32:42 +0100

    OrderOption doesn't work

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Grid,Row,Col} from 'react-flexbox-grid';
+import OrderOption from '../OrderOption/OrderOption';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
 
 
-
-const OrderForm = (cost,options) =>(
+const OrderForm = ({cost,id}) =>(
 <Grid>
   <Row>
+      {pricing.map(options =>(
+          <Col md={4} key={id}>
+              <OrderOption {...options} /> 
+          </Col>
+      ))};
     <Col xs={12}>
-      <OrderSummary tripCost={cost} tripOption={options}/>
+      <OrderSummary tripCost={cost} tripOptions={pricing}/>
     </Col>
   </Row>
 </Grid>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from '../OrderOption/OrderOption.module.scss';
+
+const OrderOption =() =>{
+    return (
+        <div>
+            <h3 className={styles.title}>Car rental</h3>
+        </div>
+    );
+};
+
+export default OrderOption;
