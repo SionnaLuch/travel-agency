f9410a3327f586c90122df4ffbc99a21fa68cfbe
Sionna Poleszuk <sionna.pol@gmail.com>
2022-02-22 20:16:24 +0100

    TDD first test 20.2

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HappyHourAd = () => {
+  return(
+    <div>
+      <HappyHourAd/>
+    </div>
+  );
+};
+
+export default HappyHourAd;
---
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import HappyHourAd from './HappyHourAd';
+
+describe('Component HappyHourAs',() => {
+  it('should render without crashing',()=>{
+    const component = shallow(<HappyHourAd/>);
+    
+    expect(component).toBeTruthy();
+  });
+
+});
