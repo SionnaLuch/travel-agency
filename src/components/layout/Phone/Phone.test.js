7867f8cba81c5fd9a65965f18203f6bcfef25953
Sionna Poleszuk <sionna.pol@gmail.com>
2022-03-03 22:34:24 +0100

    20.4 phone tests

--- src/components/layout/Phone/Phone.test.js
+++ src/components/layout/Phone/Phone.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Phone from './Phone';
+import Icon from'../../common/Icon/Icon';
 
 
 describe ('component Phone ',()=>{
@@ -8,4 +9,46 @@ describe ('component Phone ',()=>{
     const component = shallow(<Phone/>);
     expect(component).toBeTruthy();
   });
+});
+it('should contain icon',()=>{
+  const component = shallow(<Phone/>);
+  expect(component.exists(Icon)).toEqual(true);
+});
+const trueDate = Date;
+const mockDate = customDate => class extends Date {
+  constructor(...args) {
+    if (args.length) {
+      super(...args);
+    } else {
+      super(customDate);
+    }
+    return this;
+  }
+  static now() {
+    return (new Date(customDate)).getTime();
+  }
+};
+it('should show Amanda phone at 8-12',()=>{
+  global.Date = mockDate(`2019-11-19T10:00:01.135Z`);
+  const component= shallow(<Phone/>);
+  expect(component.find('span').text()).toEqual(' Amanda 678.243.8455');
+  global.Date = trueDate;
+});
+it('should show Tobias phone at 12-16',()=>{
+  global.Date = mockDate(`2019-11-19T14:00:01.135Z`);
+  const component= shallow(<Phone/>);
+  expect(component.find('span').text()).toEqual(' Tobias 278.443.6443');
+  global.Date = trueDate;
+});
+it('should show Helena phone at 16-22',()=>{
+  global.Date = mockDate(`2019-11-19T20:00:01.135Z`);
+  const component= shallow(<Phone/>);
+  expect(component.find('span').text()).toEqual(' Helena 167.280.3970');
+  global.Date = trueDate;
+});
+it('should show Helena phone at 22-8',()=>{
+  global.Date = mockDate(`2019-11-19T1:00:01.135Z`);
+  const component= shallow(<Phone/>);
+  expect(component.find('span').text()).toEqual(' The office opens at 8:00 UTC');
+  global.Date = trueDate;
 });
